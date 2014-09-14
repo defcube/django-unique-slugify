05a9e1a4ca0ec9572499f10c518d716556490e43
pgatt <gattster@gmail.com>
2014-09-14 00:59:59 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+readme = open('README.rst').read()
+
+setup(
+    name='django-unique-slugify',
+    version='1.00',
+    author_email='gattster@gmail.com',
+    author='SmileyChris (packaged by Philip Gatt)',
+    description="Automatically create a unique slug for a model.",
+    long_description=readme,
+    url='http://github.com/defcube/django-unique-slugify',
+    py_modules=['django_unique_slugify'],
+    data_files=[('', ['README.rst'])],
+    )
